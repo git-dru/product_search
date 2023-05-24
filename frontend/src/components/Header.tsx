e030e76fd4806166ddda48941bd456267c5cdd30
git-dru <mastercrypto.us@gmail.com>
2023-05-25 00:53:21 +0300

    add: create header component

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Navbar, Nav, Button } from "react-bootstrap";
+
+import { logout } from "../redux/actions/userActions";
+import { AppDispatch, RootState } from "../redux/store";
+
+const Header: React.FC = () => {
+  const email = useSelector((state: RootState) => state.auth.email);
+  const dispatch: AppDispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
+  return (
+    <Navbar
+      expand="lg"
+      className="d-flex align-items-center p-2 pb-5 mb-5 px-5"
+    >
+      <Navbar.Brand className="d-flex align-items-center">
+        Product App
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ms-auto d-flex align-items-center">
+          {email && (
+            <Nav.Item>
+              <div>{email}</div>
+            </Nav.Item>
+          )}
+          {email && (
+            <Nav.Item>
+              <Button variant="danger" onClick={handleLogout} className="ms-4">
+                Logout
+              </Button>
+            </Nav.Item>
+          )}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default Header;
